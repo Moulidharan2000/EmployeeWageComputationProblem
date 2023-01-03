4bd839c6a686774d6a582e7d7f6027077d302388
Moulidharan2000 <smoulidharanmech@gmail.com>
2023-01-03 20:24:30 +0530

    Added : Part Time Employee Wage

--- src/main/java/com/BridgeLabz/EmployeeWage/EmployeeWagesBuilder.java
+++ src/main/java/com/BridgeLabz/EmployeeWage/EmployeeWagesBuilder.java
@@ -6,12 +6,22 @@ public class EmployeeWagesBuilder {
 
 	public static void main(String[] args) {
 		int FullTime = 1;
+		int WageperHr = 20;
+		int WorkingHr = 8;
+		int PartTimeWorkingHr = 4;
+		int PartTimeEmployee = 2;
 		
 		Random random = new Random();
 		int randomNum = random.nextInt(3);
 		
-		if(randomNum == FullTime) {
+		if(FullTime == randomNum) {
+			int DailyWage = WageperHr * WorkingHr;
 			System.out.println("Employee is Present");
+			System.out.println("Daily Employee Wage is : "+DailyWage);
+		}
+		else if(PartTimeEmployee == randomNum) {
+			int PartTimeWage = PartTimeWorkingHr * WageperHr;
+			System.out.println("Part Time Employee Wage is : "+PartTimeWage);
 		}
 		else
 			System.out.println("Employee is Absent");
