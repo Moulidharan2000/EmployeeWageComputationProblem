090492a4fdb6b37e028fe336e7e94253bece5a30
Moulidharan2000 <smoulidharanmech@gmail.com>
2023-01-16 14:26:11 +0530

    Refactored : Write Class Methods to Compute Employee Wage

--- src/main/java/com/BridgeLabz/EmployeeWage/EmployeeWagesBuilder.java
+++ src/main/java/com/BridgeLabz/EmployeeWage/EmployeeWagesBuilder.java
@@ -1,51 +1,25 @@
-<<<<<<< HEAD
 package com.bridgeLabz.employeewage;
-=======
-package com.bridgelabz.employeewage;
->>>>>>> 7b7657db74ddff808728b00ae2151a23974c0b98
 
 import java.util.Random;
 
 public class EmployeeWagesBuilder {
 	
-	public static final int FULL_TIME = 1;
-	public static final int PART_TIME = 2;
+	public static final int FullTime = 1;
+	public static final int PartTime = 2;
 	
-<<<<<<< HEAD
 	static void TotalWages(int WageperHr, int WorkingHr, int TotalEmpWage, int TotalEmpHrs, int TotalHrsinMonth,
 						   int TotalWorkingDays, int NoOfWorkingDays ) {
 		
-=======
-	public static void main(String[] args) {
-		int WageperHr = 20;
-		int WorkingHr = 8;
-		int WageperHr = 20;
-		int WorkingHr = 0;
-		int NoofWorkingDays = 20;
-		int TotalEmpWage = 0;
-		int TotalHrsinMonth = 100;
-		int TotalEmpHrs = 0;
-		int TotalWorkingDays = 0;
->>>>>>> 7b7657db74ddff808728b00ae2151a23974c0b98
 		Random random = new Random();
 		while (TotalEmpHrs <= TotalHrsinMonth && TotalWorkingDays < NoOfWorkingDays ) {
 			
 		TotalWorkingDays ++;
 		int EmployeeCheck = random.nextInt(3);
 		
-<<<<<<< HEAD
 		switch(EmployeeCheck) {
 			case FullTime:
 				WorkingHr = 8;
 				break;
-=======
-		if(randomNum == FullTime) {
-			int DailyWage = WageperHr * WorkingHr;
-			System.out.println("Employee is Present");
-			System.out.println("Daily Employee Wage is : "+DailyWage);
-
-    		while (TotalEmpHrs <= TotalHrsinMonth && TotalWorkingDays < NoofWorkingDays ) {
->>>>>>> 7b7657db74ddff808728b00ae2151a23974c0b98
 			
 			case PartTime :
 				WorkingHr = 4;
